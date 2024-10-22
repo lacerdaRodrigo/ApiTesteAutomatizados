3ca30afdd653dc3f9a18adb145a5521eab345e53
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-10-22 00:14:44 -0300

    Corrigir importação do mongoose e configurar rotas

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 require('./configBd/db');
 
+// Forma de ler JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-
-// forma de ler JSON
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
-// rotas da API
-const pessoaRoutes = require('./routes/pessoa');  // Atualize o caminho aqui
+// Rotas da API
+const pessoaRoutes = require('./routes/pessoa');
 app.use('/pessoa', pessoaRoutes);
-
-const agendamentoRoutes = require('./routes/agendamento');  // Atualize o caminho aqui
+const agendamentoRoutes = require('./routes/agendamento');
 app.use('/agendamento', agendamentoRoutes);
 
-// Iniciar servidor (mova o app.listen para o arquivo principal)
+// Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-
-
-
-
