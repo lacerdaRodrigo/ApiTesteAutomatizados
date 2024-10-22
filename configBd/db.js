6d3722317c8527867522ebf5d0a48d1dc24506b0
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-10-22 00:08:06 -0300

    melhorando

--- configBd/db.js
+++ configBd/db.js
@@ -14,13 +14,13 @@
 // module.exports = mongoose;
 
 
-const mongoose = require('mongoose');
-const username = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const cluster = process.env.DB_CLUSTER;
-const dbName = process.env.MONGODB_DB_NAME;
+// const mongoose = require('mongoose');
+// const username = process.env.DB_USER;
+// const password = process.env.DB_PASSWORD;
+// const cluster = process.env.DB_CLUSTER;
+// const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority&appName=Api`;
+const uri = `mongodb+srv://lacerdaarodrigo:HH6yYxrdy1yLl6CA@api.hsggv.mongodb.net/?retryWrites=true&w=majority&appName=Api`;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado ao MongoDB Atlas'))
