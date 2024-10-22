84875aa8ec0b46201f18665a3708609b4f98aca5
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-10-21 23:38:53 -0300

    Deploy API para Heroku

--- configBd/db.js
+++ configBd/db.js
@@ -1,13 +1,27 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const mongoose = require('mongoose');
+
+// const DB_USER = process.env.DB_USER;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+
+
+// mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@api.hsggv.mongodb.net/?retryWrites=true&w=majority&appName=Api`)
+//     .then(() => {
+//         console.log('Conectamos ao MongoDB');
+//     })
+//     .catch((err) => console.log(err));
+
+// module.exports = mongoose;
+
+
 const mongoose = require('mongoose');
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const cluster = process.env.MONGODB_CLUSTER;
+const dbName = process.env.MONGODB_DB_NAME;
 
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority&appName=Api`;
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@api.hsggv.mongodb.net/?retryWrites=true&w=majority&appName=Api`)
-    .then(() => {
-        console.log('Conectamos ao MongoDB');
-    })
-    .catch((err) => console.log(err));
-
-module.exports = mongoose;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado ao MongoDB Atlas'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB', err));
