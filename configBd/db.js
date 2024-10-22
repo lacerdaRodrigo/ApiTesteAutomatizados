67b705f470c637477f53c1c27976c1379861bb79
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-10-22 00:09:23 -0300

    Atualizar configuração do MongoDB no código

--- configBd/db.js
+++ configBd/db.js
@@ -26,3 +26,4 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado ao MongoDB Atlas'))
   .catch(err => console.error('Erro ao conectar ao MongoDB', err));
 
+ 
