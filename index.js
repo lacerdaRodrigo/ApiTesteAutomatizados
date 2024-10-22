9b1d1f5326442a01a49e594bf302974565e09afc
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-10-21 23:56:14 -0300

    Adicionar rota /Pessoa

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = express()
 require('./configBd/db');
 
 
-// Iniciar servidor (mova o app.listen para o arquivo principal)
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+
 
 // forma de ler JSON
 app.use(express.urlencoded({extended: true}))
@@ -19,6 +15,12 @@ app.use('/pessoa', pessoaRoutes);
 
 const agendamentoRoutes = require('./routes/agendamento');  // Atualize o caminho aqui
 app.use('/agendamento', agendamentoRoutes);
+
+// Iniciar servidor (mova o app.listen para o arquivo principal)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 
 
