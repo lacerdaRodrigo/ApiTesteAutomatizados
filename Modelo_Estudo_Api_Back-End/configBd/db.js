3a644cbfad184d21d5547f60ec934000456888b7
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-12-12 22:39:13 -0300

    git

--- Modelo_Estudo_Api_Back-End/configBd/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const uri = 'mongodb+srv://lacerdaarodrigo:Pwd123@teste01.kokeq.mongodb.net/?retryWrites=true&w=majority&appName=TESTE01';
-
-mongoose.connect(uri)
-    .then(() => console.log('Conectado ao MongoDB Atlas'))
-    .catch(err => console.error('Erro ao conectar ao MongoDB', err));
-
-module.exports = mongoose;
